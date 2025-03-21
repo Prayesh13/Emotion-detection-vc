2cb007b040e259f17a2672c8ee76782262e46d3b
Prayesh13 <prayeshgodhani04@gmail.com>
2025-03-21 10:08:57 +0530

    add cookiecutter template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A small machine learning project to predict the emotion on given text.',
+    author='Prayesh Godhani',
+    license='',
+)
